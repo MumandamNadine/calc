4bb4c82dc8930279af32dd741451e1346ca91c89
MumandamNadine <143093141+MumandamNadine@users.noreply.github.com>
2024-04-16 21:19:54 +0530

    Add files via upload

---
+++ myindex.js
@@ -0,0 +1,59 @@
+const inputValue = document.getElementById("input");
+const number = document.querySelectorAll(".num").forEach(function (maxi) {
+  maxi.addEventListener("click", function (e) {
+    if (inputValue.innerText === "NaN") {
+      inputValue.innerText = "";
+    }
+    if (inputValue.innerText === "0") {
+      inputValue.innerText = "";
+    }
+    inputValue.innerText += e.target.innerHTML.trim();
+  });
+});
+const calculate = document.querySelectorAll(".itemfxn").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      const mynum = inputValue.innerText;
+      let lastValue = inputValue.innerText.substring(inputValue.innerText.length - 1);
+      
+      if (e.target.innerHTML === "clear") {
+        inputValue.innerText = '';
+      } else if (inputValue.innerText === "") {
+        // Handle empty input case
+        inputValue.innerText = "0";
+      } else {
+        if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+            inputValue.innerText = eval(inputValue.innerText);
+          }
+          else if (e.target.innerHTML === "x²") {
+            inputValue.innerText = eval((inputValue.innerText)*(inputValue.innerText));
+          }
+          else if (e.target.innerHTML === "clear") {
+            inputValue.innerText = '';
+          }
+          else if (e.target.innerHTML === "√") {
+            inputValue.innerText = eval(Math.sqrt(inputValue.innerText));
+          }
+          else if (e.target.innerHTML === "log()") {
+            inputValue.innerText = eval(Math.log10(inputValue.innerText))
+          }
+          else if (e.target.innerHTML === "exp()") {
+            inputValue.innerText = eval(Math.exp(inputValue.innerText))
+          }
+          else if(e.target.innerHTML === "+/-")
+          {
+            inputValue.innerText += '-'
+          }
+          else if (e.target.innerHTML === "del") {
+            inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
+            if (inputValue.innerText.length == 0) {
+              inputValue.innerText = '';
+            }
+          } else {
+            if (!isNaN(lastValue)) {
+              inputValue.innerText += e.target.innerHTML;
+            }
+          }
+      }
+    });
+  });
+  
